ce135005dcd64650d8a243e4b3ac52dd45c6afed
j-dimension <info@j-lawyer.org>
2020-01-26 23:06:39 +0100

    dummy changes

--- src/1.12.0.0/miete01_meta.groovy
+++ src/1.12.0.0/miete01_meta.groovy
@@ -664,7 +664,7 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "RVG-Rechner"
 description = "j-lawyer.org RVG-Rechner"
-version = "0.2.0";
+version = "0.1.0";
 author = "Anatol Anuschewski, Jens Kutschke"
 updated = "18.11.2019"
 supportedPlaceHolders = "{{RVG_TABELLE}}, {{RVG}}"

b34eaf7804b266fef04a79d8a214efc6b6a7dacc
j-dimension <info@j-lawyer.org>
2020-01-31 21:11:26 +0100

    Introducing form libraries

--- src/1.12.0.0/verkehr01_meta.groovy
+++ src/1.12.0.0/verkehr01_meta.groovy
@@ -664,8 +664,8 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "Verkehrsunfalldaten"
 description = "Verkehrsunfalldaten"
-version = "0.5.0";
+version = "0.6.0";
 author = "Jens Kutschke"
-updated = "27.01.2020"
+updated = "31.01.2020"
 
 

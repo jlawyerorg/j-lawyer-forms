e9699d734dbcf08190643bdff52874e0c0878225
j-dimension <info@j-lawyer.org>
2023-03-22 22:27:35 +0100

    layout fix to avoid form becoming too wide

--- src/2.1.0.0/gravity01_meta.groovy
+++ src/2.1.0.0/gravity01_meta.groovy
@@ -664,6 +664,6 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "Gravity Forms"
 description = "Uebernimmt Eingaben aus Wordpress Gravity Forms"
-version = "0.5.1";
+version = "0.5.2";
 author = "Jens Kutschke"
 updated = "22.03.2023"

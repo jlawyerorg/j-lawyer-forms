3d7c1ebdff7c55f35e1bbcbb446561bb310b3dcb
j-dimension <info@j-lawyer.org>
2025-09-04 12:55:18 +0200

    UVZ export refinements wip

--- src/2.6.0.0/notariat02_meta.groovy
+++ src/2.6.0.0/notariat02_meta.groovy
@@ -547,6 +547,6 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "Notariat: UVZ"
 description = "Falldatenblatt für Eintragungen im Urkundenverzeichnis"
-version = "0.9.0";
+version = "0.10.0";
 author = "Jens Kutschke"
-updated = "03.09.2025"
+updated = "04.09.2025"

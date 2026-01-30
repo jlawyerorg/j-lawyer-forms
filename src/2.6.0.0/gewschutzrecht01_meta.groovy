188454196887f9987b53d7516f3c450bf5e74d54
j-dimension <info@j-lawyer.org>
2026-01-30 18:05:31 +0100

    gewschutzrecht01 0.3.0

--- src/2.6.0.0/gewschutzrecht01_meta.groovy
+++ src/2.6.0.0/gewschutzrecht01_meta.groovy
@@ -547,6 +547,6 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "gew. Schutzrecht"
 description = "gewerbliches Schutzrecht"
-version = "0.2.1";
+version = "0.3.0";
 author = "Jens Kutschke"
-updated = "09.01.2026"
+updated = "30.01.2026"
